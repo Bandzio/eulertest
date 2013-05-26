3658c8a33770b6925306779aefa0cf2fdabed41d
Marcin Banaszynski <mbinne@gmail.com>
2013-05-26 12:30:32 +0200

    add comment

---
+++ Euler_10/src/pl/com/marban/Euler10.java
@@ -0,0 +1,43 @@
+package pl.com.marban;
+
+public class Euler10 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		long startTime = System.currentTimeMillis();
+		
+		//jakis tam komentarz
+		sitoEratostenesa(2000000);
+		
+		long endTime = System.currentTimeMillis();
+		
+		
+		System.out.print(" czas zajety na obliczenia :" +(endTime-startTime) + " [miliseconds] ,  "+(endTime-startTime)/1000+" [seconds]");
+	}
+
+
+	private static void sitoEratostenesa(int base) {
+		int n = base;
+        boolean[] numbersTable = new boolean[n+1];
+        for(int i = 2; i*i <= n; i++)
+        {
+            if (numbersTable[i] == true)
+                continue;
+            for (int j = 2 * i ; j <= n; j += i)
+                numbersTable[j] = true;
+ 
+        }
+        System.out.println("Liczby pierwsze z przedzia³u od 2 do " + n + ":");
+        long sum = 0;
+        for (int i = 2; i <= n; i++)
+            if (numbersTable[i] == false){
+            	sum = sum + i;
+            }
+ 
+		
+		System.out.print(" suma pierwszych dla "+base+" wynosi "+sum);
+	}
+}
