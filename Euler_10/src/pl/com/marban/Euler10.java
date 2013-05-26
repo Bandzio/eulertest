d3e7f5bc627841a45240690cd3f2fbe02ce10175
Marcin Banaszynski <mbinne@gmail.com>
2013-05-26 12:40:27 +0200

    N

--- Euler_10/src/pl/com/marban/Euler10.java
+++ Euler_10/src/pl/com/marban/Euler10.java
@@ -20,19 +20,19 @@ public class Euler10 {
 
 
 	private static void sitoEratostenesa(int base) {
-		int n = base;
-        boolean[] numbersTable = new boolean[n+1];
-        for(int i = 2; i*i <= n; i++)
+		int N = base;
+        boolean[] numbersTable = new boolean[N+1];
+        for(int i = 2; i*i <= N; i++)
         {
             if (numbersTable[i] == true)
                 continue;
-            for (int j = 2 * i ; j <= n; j += i)
+            for (int j = 2 * i ; j <= N; j += i)
                 numbersTable[j] = true;
  
         }
-        System.out.println("Liczby pierwsze z przedzia³u od 2 do " + n + ":");
+        System.out.println("Liczby pierwsze z przedzia³u od 2 do " + N + ":");
         long sum = 0;
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i <= N; i++)
             if (numbersTable[i] == false){
             	sum = sum + i;
             }
